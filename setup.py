f008cd88bd4e79cb88a4be5a9d89489245a8709c
Parag <paraglondhe123@gmail.com>
2024-12-19 12:46:56 +0530

    m

--- setup.py
+++ setup.py
@@ -2,33 +2,36 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Training module for training PyTorch models'
+LONG_DESCRIPTION = 'A versatile PyTorch training framework to simplify and enhance the model training process. It includes a trainer class with efficient training methods, famous built in pre-trained architectures, metrics tracking, custom and built-in callbacks support, and much more!'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="pytorch-candle",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Parag Londhe",
+    author_email="<paraglondhe123@gmail.com>",
     description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url="https://github.com/paraglondhe/pytorch-candle",
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=[
+        'torch>=1.9.0',
+        'matplotlib',
+        'tqdm',
+        'torchsummary'
+    ],
+    keywords=['python', 'pytorch', 'model training', 'deep learning', 'AI'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
-    ]
+    ],
+    python_requires='>=3.7',
 )
