b8a82fc8aa209aeedd63ea43daa47458cf276f23
Parag <paraglondhe123@gmail.com>
2025-01-12 21:14:57 +0530

    added blueprint

--- candle/trainer.py
+++ candle/trainer.py
@@ -9,8 +9,7 @@ from candle.callbacks import Callback, CallbacksList
 from candle.metrics import Metric
 import torch
 import copy
-from torchsummary import summary
-from typing import Tuple, Optional, List, Callable, Dict
+from typing import Optional, List, Callable, Dict
 from tqdm import tqdm
 from datetime import datetime
 import os
@@ -31,22 +30,22 @@ class TrainerModule(Module):
         pass
 
 
-class Trainer(TrainerModule):
-    def __init__(self, model: torch.nn.Module,
-                 criterion: torch.nn.Module,
-                 input_shape: Tuple[int, ...],
-                 optimizer: torch.optim.Optimizer,
-                 metrics: Optional[List[Metric]] = None,
+class TrainerBluePrint(TrainerModule):
+    def __init__(self,
                  callbacks: Optional[List[Callback]] = None,
                  display_time_elapsed: bool = False,
                  roff: int = 5,
                  report_in_one_line: bool = True,
                  clear_cuda_cache: bool = True,
                  use_amp: bool = True,
+                 name: Optional[str] = None,
                  device: Optional[torch.device] = None,
-                 logger: Optional[logging.Logger] = None):
+                 logger: Optional[logging.Logger] = None,
+                 display_blueprint_use_warning: bool = True):
 
-        super().__init__(name="SimpleTrainer", device=(device or torch.device('cpu')), logger=logger)
+        super().__init__(name=name, device=(device or torch.device('cpu')), logger=logger)
+        self.best_model_weights = None
+        self.final_metrics = None
         self.epochs = None
         self.messages_joiner = "  ||  " if report_in_one_line else "\n"
         self.epoch_message = ""
@@ -56,26 +55,16 @@ class Trainer(TrainerModule):
         self.__current_epoch = 0
         self.__current_batch = 0
 
-        self.metrics = [metric.name for metric in metrics]
-        self.metric_fns = {metric.name: metric for metric in
-                           metrics}
-
-        self.model = self.to_device(model)
-        self.criterion = criterion
-        self.optimizer = optimizer
         self.clear_cuda_cache = clear_cuda_cache
         self.use_amp = use_amp and self.device.type == 'cuda'
         self.scaler = GradScaler(enabled=self.use_amp)
         self.tracker = self.init_tracker()
 
         self.roff = roff
-        self.input_shape = input_shape
         self.display_time_elapsed = display_time_elapsed
 
         self.STOPPER = False
         self.external_events = set()
-        self.best_model_weights = None
-        self.final_metrics = {}
 
         self.std_pos = {'on_train_batch_begin', 'on_train_batch_end', 'on_epoch_begin', 'on_epoch_end',
                         'on_test_batch_begin', 'on_test_batch_end', 'on_predict_batch_begin', 'on_predict_batch_end',
@@ -83,19 +72,25 @@ class Trainer(TrainerModule):
                         'on_predict_end', 'before_training_starts', 'after_training_ends', 'before_backward_pass'}
 
         self.callbacks = CallbacksList(callbacks=callbacks, trainer=self)
+        if display_blueprint_use_warning:
+            self.blueprint_use_warning()
 
+    def blueprint_use_warning(self):
+        warning_message = (
+            "You have inherited from the 'TrainerBluePrint' class. "
+            "Ensure that you have implemented all the required callback executions, "
+            "as missing or incorrect implementations can lead to unexpected behavior.\n\n"
+            "The following standard callback positions are available:\n"
+            f"{', '.join(sorted(self.std_pos))}\n\n"
+            "Callbacks allow you to customize and extend the training process at various stages. "
+            "You can add your own custom callbacks and specify their positions using these standard positions or by defining new ones.\n\n"
+            "To disable this warning in future runs, set the attribute 'display_blueprint_use_warning' to False."
+        )
+        self.logger.warning(warning_message)
+
+    @abstractmethod
     def init_tracker(self):
-        temp = self.metrics + ["loss"]
-        metrics = []
-        for metric in temp:
-            metrics.append(metric)
-            metrics.append(f"val_{metric}")
-        tracker = Tracker(metrics)
-        tracker.logger = self.logger
-        return tracker
-
-    def model_summary(self):
-        summary(self.model, self.input_shape)
+        pass
 
     def add_callback(self, callback: Callback) -> None:
         """
@@ -122,52 +117,16 @@ class Trainer(TrainerModule):
         """
         self.callbacks.remove(callback)
 
-    def __run_callbacks(self, pos: str) -> List[Optional[str]]:
+    def _run_callbacks(self, pos: str) -> List[Optional[str]]:
         return self.callbacks.run_all(pos)
 
-    def __train_fn(self, train_loader: torch.utils.data.DataLoader) -> None:
+    @abstractmethod
+    def train(self, train_loader: torch.utils.data.DataLoader):
+        pass
 
-        # Set to training mode
-        self.model.train()
-        self.__run_callbacks(pos="on_train_begin")
-        for self.__current_batch, (inputs, labels) in tqdm(enumerate(train_loader), self.epoch_message):
-            inputs, labels = inputs.to(self.device), labels.to(self.device)
-            self.__run_callbacks(pos="on_train_batch_begin")
-
-            # One Batch Training
-            self.optimizer.zero_grad()
-
-            with autocast(device_type=self.device.type, enabled=self.use_amp):
-                outputs = self.model(inputs)
-                loss = self.criterion(outputs, labels)
-            self.__run_callbacks(pos="before_backward_pass")
-            self.scaler.scale(loss).backward()
-            self.scaler.step(self.optimizer)
-            self.scaler.update()
-
-            with torch.no_grad():
-                self.tracker.update({"loss": loss.item()})
-                self.tracker.update({metric: self.metric_fns[metric](labels, outputs) for metric in self.metrics})
-            self.__run_callbacks(pos="on_train_batch_end")
-        self.__run_callbacks(pos="on_train_end")
-
-    @torch.no_grad()
-    def __validation_fn(self, val_loader: torch.utils.data.DataLoader) -> None:
-        # Set to the evaluation mode
-        self.model.eval()
-        self.__run_callbacks(pos="on_test_begin")
-        for inputs, labels in val_loader:
-            self.__run_callbacks(pos="on_test_batch_begin")
-            inputs, labels = inputs.to(self.device), labels.to(self.device)
-            with autocast(device_type=self.device.type, enabled=self.use_amp):
-                outputs = self.model(inputs)
-                val_loss = self.criterion(outputs, labels)
-
-            self.tracker.update({"val_loss": val_loss.item()})
-            self.tracker.update(
-                {"val_" + metric: self.metric_fns[metric](labels, outputs) for metric in self.metrics})
-            self.__run_callbacks(pos="on_test_batch_end")
-        self.__run_callbacks(pos="on_test_end")
+    @abstractmethod
+    def validate(self, val_loader: torch.utils.data.DataLoader):
+        pass
 
     @property
     def current_batch(self):
@@ -178,7 +137,7 @@ class Trainer(TrainerModule):
         return self.__current_epoch
 
     def fit(self, train_loader: torch.utils.data.DataLoader, val_loader: torch.utils.data.DataLoader,
-            epochs: int = 1, epoch_start: int = 0):
+            epochs: int = 1, epoch_start: int = 0) -> Dict:
         """
         Trains the model for the specified number of epochs.
 
@@ -189,7 +148,7 @@ class Trainer(TrainerModule):
             epochs (int): No. of epochs to run for
 
         Returns:
-            None
+            Dict: History of training and validation metrics, and other information.
         """
         self.epochs = epochs
         self.num_batches = len(train_loader)
@@ -199,9 +158,9 @@ class Trainer(TrainerModule):
         # The main Training loop
 
         start_time = time.time()
-        self.__run_callbacks(pos="before_training_starts")
+        self._run_callbacks(pos="before_training_starts")
         for self.__current_epoch in range(epoch_start, epoch_start + self.epochs):
-            self.__run_callbacks(pos="on_epoch_begin")
+            self._run_callbacks(pos="on_epoch_begin")
 
             if on_gpu and self.clear_cuda_cache:
                 torch.cuda.empty_cache()
@@ -209,13 +168,13 @@ class Trainer(TrainerModule):
             self.epoch_message = f"EPOCH {self.current_epoch}: "
 
             # Train model
-            self.__train_fn(train_loader)
-            self.__validation_fn(val_loader)
+            self.train(train_loader)
+            self.validate(val_loader)
             epoch_statistics = tracker.message("--> Metrics: ")
             self.logger.info(epoch_statistics)
 
             # Run callbacks
-            self.__run_callbacks(pos="on_epoch_end")
+            self._run_callbacks(pos="on_epoch_end")
 
             tracker.snap_and_reset_all()
 
@@ -227,10 +186,137 @@ class Trainer(TrainerModule):
                 break
 
         self.final_metrics = self.tracker.get_final_values(self.current_epoch)
-        self.__run_callbacks(pos="after_training_ends")
+        self._run_callbacks(pos="after_training_ends")
         if self.best_model_weights is None:
             self.best_model_weights = copy.deepcopy(self.get_model_weights())
         return tracker.get_history()
+
+    @abstractmethod
+    def get_model_weights(self):
+        pass
+
+    @abstractmethod
+    def predict(self, data_loader: torch.utils.data.DataLoader) -> torch.Tensor:
+        pass
+
+    class __CallbackTemplate(Callback):
+        def __init__(self):
+            super().__init__()
+
+    def add_event(self, pos: str):
+        """
+        Write a custom callback event without explicitly creating a new callback class.
+        """
+
+        def decorator(event: Callable) -> Optional[Callable]:
+            # Check if the event is already registered
+            if event.__name__ in self.external_events:
+                return None  # Do nothing if event already exists
+
+            # Create a new callback template
+            ct = self.__CallbackTemplate()
+
+            # Register the event if the position is valid
+            if pos in self.std_pos:
+                setattr(ct, pos, event)
+            else:
+                raise AttributeError(f"Invalid method '{pos}'. Must be one of {self.std_pos}.")
+
+            # Add the callback template to the callback list
+            self.external_events.add(event.__name__)
+            self.add_callback(ct)
+
+            return event
+
+        return decorator
+
+
+class Trainer(TrainerBluePrint):
+    def __init__(self, model: torch.nn.Module,
+                 criterion: torch.nn.Module,
+                 optimizer: torch.optim.Optimizer,
+                 metrics: Optional[List[Metric]] = None,
+                 callbacks: Optional[List[Callback]] = None,
+                 display_time_elapsed: bool = False,
+                 roff: int = 5,
+                 report_in_one_line: bool = True,
+                 clear_cuda_cache: bool = True,
+                 use_amp: bool = True,
+                 device: Optional[torch.device] = None,
+                 logger: Optional[logging.Logger] = None):
+
+        self.metrics = [metric.name for metric in metrics]
+        self.metric_fns = {metric.name: metric for metric in
+                           metrics}
+        super().__init__(
+            callbacks=callbacks,
+            display_time_elapsed=display_time_elapsed,
+            roff=roff,
+            report_in_one_line=report_in_one_line,
+            clear_cuda_cache=clear_cuda_cache,
+            use_amp=use_amp,
+            name="SimpleTrainer",
+            device=(device or torch.device('cpu')),
+            logger=logger,
+            display_blueprint_use_warning=False
+        )
+        self.model = self.to_device(model)
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.final_metrics = {}
+
+    def init_tracker(self):
+        temp = self.metrics + ["loss"]
+        metrics = []
+        for metric in temp:
+            metrics.append(metric)
+            metrics.append(f"val_{metric}")
+        tracker = Tracker(metrics)
+        tracker.logger = self.logger
+        return tracker
+
+    def train(self, train_loader: torch.utils.data.DataLoader) -> None:
+        # Set to training mode
+        self.model.train()
+        self._run_callbacks(pos="on_train_begin")
+        for self.__current_batch, (inputs, labels) in tqdm(enumerate(train_loader), self.epoch_message):
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            self._run_callbacks(pos="on_train_batch_begin")
+
+            # One Batch Training
+            self.optimizer.zero_grad()
+
+            with autocast(device_type=self.device.type, enabled=self.use_amp):
+                outputs = self.model(inputs)
+                loss = self.criterion(outputs, labels)
+            self._run_callbacks(pos="before_backward_pass")
+            self.scaler.scale(loss).backward()
+            self.scaler.step(self.optimizer)
+            self.scaler.update()
+
+            with torch.no_grad():
+                self.tracker.update({"loss": loss.item()})
+                self.tracker.update({metric: self.metric_fns[metric](labels, outputs) for metric in self.metrics})
+            self._run_callbacks(pos="on_train_batch_end")
+        self._run_callbacks(pos="on_train_end")
+
+    @torch.no_grad()
+    def validate(self, val_loader: torch.utils.data.DataLoader) -> None:
+        # Set to the evaluation mode
+        self.model.eval()
+        self._run_callbacks(pos="on_test_begin")
+        for inputs, labels in val_loader:
+            self._run_callbacks(pos="on_test_batch_begin")
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+            with autocast(device_type=self.device.type, enabled=self.use_amp):
+                outputs = self.model(inputs)
+                val_loss = self.criterion(outputs, labels)
+
+            self.tracker.update({"val_loss": val_loss.item()})
+            self.tracker.update(
+                {"val_" + metric: self.metric_fns[metric](labels, outputs) for metric in self.metrics})
+            self._run_callbacks(pos="on_test_batch_end")
+        self._run_callbacks(pos="on_test_end")
 
     def get_model_weights(self):
         return self.model.state_dict()
@@ -246,18 +332,18 @@ class Trainer(TrainerModule):
             torch.Tensor: Concatenated model predictions for all input batches.
         """
         self.model.eval()
-        self.__run_callbacks(pos="on_predict_begin")
+        self._run_callbacks(pos="on_predict_begin")
 
         all_predictions = []
         for batch_idx, data in enumerate(data_loader):
-            self.__run_callbacks(pos="on_predict_batch_begin")
+            self._run_callbacks(pos="on_predict_batch_begin")
             data = data.to(self.device)
             predictions = self.model(data)
             all_predictions.append(predictions)
-            self.__run_callbacks(pos="on_predict_batch_end")
+            self._run_callbacks(pos="on_predict_batch_end")
 
         all_predictions = torch.cat(all_predictions, dim=0)
-        self.__run_callbacks(pos="on_predict_end")
+        self._run_callbacks(pos="on_predict_end")
         return all_predictions
 
     def save_progress(self, path: str, metric_name: str = "val_loss", save_trainer: bool = False):
@@ -371,33 +457,6 @@ class Trainer(TrainerModule):
     class __CallbackTemplate(Callback):
         def __init__(self):
             super().__init__()
-
-    def add_event(self, pos: str):
-        """
-        Write a custom callback event without explicitly creating a new callback class.
-        """
-
-        def decorator(event: Callable) -> Optional[Callable]:
-            # Check if the event is already registered
-            if event.__name__ in self.external_events:
-                return None  # Do nothing if event already exists
-
-            # Create a new callback template
-            ct = self.__CallbackTemplate()
-
-            # Register the event if the position is valid
-            if pos in self.std_pos:
-                setattr(ct, pos, event)
-            else:
-                raise AttributeError(f"Invalid method '{pos}'. Must be one of {self.std_pos}.")
-
-            # Add the callback template to the callback list
-            self.external_events.add(event.__name__)
-            self.add_callback(ct)
-
-            return event
-
-        return decorator
 
 
 class AdversarialTrainer(TrainerModule):
